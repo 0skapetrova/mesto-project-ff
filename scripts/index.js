2c9c3fdf1314d01ea3604ac4d451a40158834ea5
Olga Petrova <oskapetrovaa@yandex.ru>
2024-06-06 15:01:26 +0300

    написан скрипт добавления и удаления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,18 @@
-// @todo: Темплейт карточки
+function createCard(i) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+        cardElement.querySelector('.card__image').src = initialCards[i].link;
+        cardElement.querySelector('.card__image').alt = initialCards[i].name;
+    const cardsList = document.querySelector('.places__list');
+        cardsList.append(cardElement);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+        deleteButton.addEventListener('click', function (evt) {
+            const eventTarget = evt.target;
+            const listItem = eventTarget.closest('.places__item');
+            listItem.remove();
+        });
+}
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+for (let i = 0; i < initialCards.length; i++) {
+    createCard(i);
+}
