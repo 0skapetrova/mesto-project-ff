ab275dc9749f4de76aa4dab3ea9386544fc16577
Olga Petrova <oskapetrovaa@yandex.ru>
2024-07-03 14:53:51 +0300

    исправлены ошибки по чеклисту

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,9 +1,5 @@
-import { deleteCard, like } from "./cards";
-import { popupEdit, popupAdd, cardTemplate, cardsList } from "../index";
-import { createCard } from "./cards";
-
 //открывает попап и добавляет слушатели закрытия для оверлея и Esc
-function popupOpen (popup) {
+function openPopup (popup) {
     popup.classList.add('popup_is-animated');
     const open = (popup) => {
         popup.classList.add('popup_is-opened');
@@ -14,7 +10,7 @@ function popupOpen (popup) {
 };
 
 //закрывает попап и снимает слушатели
-function popupClose (popup) {
+function closePopup (popup) {
     popup.classList.remove('popup_is-opened');
     const notAnimated = (popup) => {
         popup.classList.remove('popup_is-animated');
@@ -27,95 +23,23 @@ function popupClose (popup) {
 //обработчик клика по оверлею
 function handleOverlayClick (evt) {
     const closestPopup = evt.target.parentElement.closest('.popup');
-        const popupIsOpened = document.querySelector('.popup_is-opened');
+    const popupIsOpened = document.querySelector('.popup_is-opened');
+    const closestForm = popupIsOpened.querySelector('.popup__form');
         //проверяем, что клик был вне попапа и что есть открытый попап
         if (!closestPopup && popupIsOpened) {
-          popupClose(popupIsOpened);          
+          closePopup(popupIsOpened);    
+          closestForm.reset();      
         };
 };
 
-
 //обработчик нажатия Escape
 function handleEsc (evt) {
-    if (evt.code === "Escape") {  
-        const popupIsOpened = document.querySelector('.popup_is-opened');
-        popupClose(popupIsOpened);        
+    const popupIsOpened = document.querySelector('.popup_is-opened');
+    const closestForm = popupIsOpened.querySelector('.popup__form');
+    if (evt.code === "Escape") {         
+        closePopup(popupIsOpened);   
+        closestForm.reset();     
       };
 };
 
-//находим форму редактирования профиля  
-const profileForm = document.querySelector('form[name="edit-profile"]');
-
-//обработчик submit формы редактирования профиля
-function handleProfileFormSubmit(evt, close) {
-    evt.preventDefault(); 
-
-    const nameInput = profileForm.querySelector('.popup__input_type_name');
-    const jobInput = profileForm.querySelector('.popup__input_type_description');
-
-    const profileName = nameInput.value;
-    const job = jobInput.value;
-
-    // находим контейнер с данными профиля
-    const profileInfo = document.querySelector('.profile__info');
-    const nameField = profileInfo.querySelector('.profile__title');
-    const jobField = profileInfo.querySelector('.profile__description');
-
-    nameField.textContent = profileName;
-    jobField.textContent = job;
-
-    nameInput.value = '';
-    jobInput.value = '';
-    close(popupEdit);
-};
-
-//добавляем слушатель на кнопку сохранения
-profileForm.addEventListener('submit', function(evt) {
-    handleProfileFormSubmit(evt, popupClose)
-  });
-
-//находим форму добавления карточки
-const imageForm = document.querySelector('form[name="new-place"]');
-
-//обработчик submit формы добавления карточки
-function handleImageFormSubmit(evt, list, createCard, template, close) {
-    evt.preventDefault(); 
-
-    const nameInput = imageForm.querySelector('.popup__input_type_card-name');
-    const urlInput = imageForm.querySelector('.popup__input_type_url');
-
-    const cardName = nameInput.value;
-    const url = urlInput.value;
-
-    //объект, в котором хранятся данные для карточки
-    const cardDescription = {
-        link: url,
-        name: cardName,
-    };
-
-    //добавляем новую карточку в начало списка
-    list.prepend(createCard(template, cardDescription, deleteCard, like, popupImageOpen));
-    
-    nameInput.value = '';
-    urlInput.value = '';
-    close(popupAdd);
-};
-
-//добавляем слушатель на кнопку сохранения
-
-imageForm.addEventListener('submit', function(evt) {
-    handleImageFormSubmit(evt, cardsList, createCard, cardTemplate, popupClose);
-});
-
-//открывает просмотр картинки
-function popupImageOpen( cardImage ) {
-    const popupImage = document.querySelector('.popup_type_image');
-    const image = popupImage.querySelector('.popup__image');
-    image.src = cardImage.src;
-    image.alt = cardImage.alt;
-    const popupCaption = popupImage.querySelector('.popup__caption');
-    popupCaption.textContent = cardImage.alt;
-    popupOpen(popupImage);
-  };
-
-export { popupOpen, popupClose, popupImageOpen };
+export { openPopup, closePopup };
